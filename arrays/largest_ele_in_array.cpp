e057ecd78484587134e4331c01dcfba497771d27
Yashwanth Chandolu <0xyashwanth@gmail.com>
2022-11-16 18:05:07 +0530

    arrays

---
+++ arrays/largest_ele_in_array.cpp
@@ -0,0 +1,20 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int large(int arr[],int size)
+{
+    int largst=arr[0];
+    for(int i=0;i<size;i++)
+    {
+        if(arr[i]>=largst)
+        {
+            largst=arr[i];
+        }
+    }
+    return largst;
+}
+int main() {
+    int arr[5]={1,2,113,4,90};
+    cout<<large(arr,5);
+    return 0;
+}
