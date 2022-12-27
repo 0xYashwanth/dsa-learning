665883b77a442ea819c33a15566cfde3efdcdeb7
Yashwanth Chandolu <0xyashwanth@gmail.com>
2022-12-27 13:06:19 +0530

    init commit

---
+++ sorting/mergeTwoSortArr.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+mergeTwoSortArr(int a[],int b[],int m,int n)
+{
+    int i=0,j=0;
+    while(i<m&&j<n)
+    {
+        if(a[i]<b[j])
+        {cout<<a[i]<<" ";i++;}
+        else
+        {cout<<b[j]<<" ";j++;}
+    }
+    while(i<m)
+    {cout<<a[i]<<" ";i++;}
+    while(j<n)
+    {cout<<b[j]<<" ";j++;}
+}
+int main()
+{
+    return 0;
+}
