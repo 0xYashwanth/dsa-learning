84c605671158c5b7460258833157b2a7aa58ddcc
Yashwanth Chandolu <0xyashwanth@gmail.com>
2022-12-31 19:25:42 +0530

    init comit

---
+++ sorting/hoarePart.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int hoare(int arr[],int l,int h)
+{
+    int pivot=arr[l];
+    int i=l-1,j=h+1;
+    while(true)
+    {
+        do
+        {
+            i++;
+        }while(arr[i]<pivot);
+        do
+        {
+            j--;
+        } while (arr[j]>pivot);
+        if(i>j) return j;
+        swap(arr[i],arr[j]);   
+    }
+}
+int main()
+{
+    return 0;
+}
