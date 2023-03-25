0762d41236a602b0ef4104c95bf64bb0e0077141
Yashwanth Chandolu <0xyashwanth@gmail.com>
2023-03-25 18:47:17 +0530

    init commit

---
+++ strings/isSubSeq.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isSubSeq(string s1,string s2,int m,int n)
+{
+    int j=0;
+    if(m<n) return false;
+    for(int i=0;i<m&&j<n;i++)
+    {
+        if(s1[i]==s2[j]) j++;
+    }
+    return (j==n);
+}
+int main()
+{
+    return 0;
+}
