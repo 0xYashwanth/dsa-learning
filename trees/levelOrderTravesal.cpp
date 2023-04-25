6e5843fb42a49b06381f620c945ea118395d2f91
Yashwanth Chandolu <0xyashwanth@gmail.com>
2023-04-26 00:24:47 +0530

    Level Order Traversal

--- trees/levelOrderTravesal.cpp
+++ trees/levelOrderTravesal.cpp
@@ -1,35 +1,46 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
 struct Node
 {
+    int key;
     struct Node *left;
     struct Node *right;
-    int key;
-    Node(int x)
+    Node(int k)
     {
-        key=x;
-        left=NULL;
-        right=NULL;
+        key = k;
+        left = right = NULL;
     }
 };
-void levelOrder(Node *root)
+
+void printLevel(Node *root)
 {
-    if(root==NULL) return;
-    if(root->left!=NULL) cout<<root->left->key<<" ";
-    if(root->right!=NULL) cout<<root->right->key<<" ";
-    levelOrder(root->left);
-    levelOrder(root->right);
+    if (root == NULL)
+        return;
+    queue<Node *> q;
+    q.push(root);
+    while (q.empty() == false)
+    {
+        Node *curr = q.front();
+        q.pop();
+        cout << curr->key << " ";
+        if (curr->left != NULL)
+            q.push(curr->left);
+        if (curr->right != NULL)
+            q.push(curr->right);
+    }
 }
+
 int main()
 {
-    Node *root=new Node(10);
-	root->left=new Node(20);
-	root->right=new Node(30);
-	root->left->left=new Node(40);
-	root->left->right=new Node(50);
-	root->right->right=new Node(70);
-	root->right->right->right=new Node(80);
-    cout<<root->key<<" ";
-    levelOrder(root);
-    return 0;
+
+    Node *root = new Node(10);
+    root->left = new Node(20);
+    root->right = new Node(30);
+    root->left->left = new Node(40);
+    root->left->right = new Node(50);
+    root->right->left = new Node(60);
+    root->right->right = new Node(70);
+
+    printLevel(root);
 }
