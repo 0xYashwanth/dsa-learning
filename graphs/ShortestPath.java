14aa53e91d7f696ed23026768745d7e678a7b84a
Yashwanth Chandolu <0xyashwanth@gmail.com>
2023-08-03 01:25:12 +0530

    dijkstras

--- graphs/ShortestPath.java
+++ graphs/ShortestPath.java
@@ -33,7 +33,6 @@ class ShortestPath {
 			sptSet[u] = true;
 			for (int v = 0; v < V; v++)
 				if (!sptSet[v] && graph[u][v] != 0
-					&& dist[u] != Integer.MAX_VALUE
 					&& dist[u] + graph[u][v] < dist[v])
 					dist[v] = dist[u] + graph[u][v];
 		}
